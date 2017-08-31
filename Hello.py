e2e8e1410f855f1cf8dd94ee44586ae50ac69e51
InfamousHacker4chan <ajones2964@greenvilleschools.us>
2017-08-31 08:43:15 -0400

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,12 @@
+# This program says hello and greets a person by name.
+#
+# Ashton Jones
+# August 24, 2017
+
+print("Hello.")
+print("What is your name?")
+name = input()
+print("It is good to meet you, " + name + ".")
+print("Where were you born, " + name + "?")
+location = input()
+print("That's interesting. I'd like to visit " + location + " one day.")
---
+++ madlib.py
@@ -0,0 +1,8 @@
+print("Give me an adjective")
+adjective1 = input()
+print("Give me a noun")
+noun1 = input()
+print("Give me an action verb")
+verb1 = input()
+
+print("The " + adjective1 + " " + noun1 + " was " + verb1 + " from the tree.")
